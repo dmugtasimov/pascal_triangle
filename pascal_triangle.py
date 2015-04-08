0a0192434db1a5c3bfe0a5856c6c542e3e1ee3df
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2015-04-09 01:11:59 +0300

    More versions of the algorithm

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,5 +1,6 @@
 import timeit
 import itertools
+import collections
 
 
 def print_pascal_triangle(n):
@@ -70,17 +71,79 @@ def print_pascal_triangle_v5(height):
     return line
 
 
-#print_pascal_triangle_v5(5)
-#import sys
-#sys.exit(1)
-FUNCS = ('print_pascal_triangle',
-         'print_pascal_triangle_v2',
-         'print_pascal_triangle_v3',
-         'print_pascal_triangle_v4',
-         'print_pascal_triangle_v5')
-CYCLES = 10000
-HEIGHT = 20
-for func in FUNCS:
-    print '%s(%s) done for %s times in %s seconds' % (func, HEIGHT, CYCLES,
-            timeit.timeit('%s(%s)' % (func, HEIGHT), setup='from __main__ import %s' % func,
-                          number=CYCLES))
+def print_pascal_triangle_v6(height):
+    line = []
+    for x in xrange(height):
+        line = [1] + line
+        for i in xrange(1, len(line)):
+            try:
+                line[i] += line[i + 1]
+            except IndexError:
+                pass
+        #print line
+
+
+def print_pascal_triangle_v7(height):
+    line = collections.deque()
+    for x in xrange(height):
+        line.appendleft(1)
+        for i in xrange(1, len(line)):
+            try:
+                line[i] += line[i + 1]
+            except IndexError:
+                pass
+        #print line
+
+
+#@profile
+def print_pascal_triangle_v8(height):
+    line = collections.deque()
+    x = 0
+    while x < height:
+        line.appendleft(1)
+        i = 1
+        while i <= x:
+            try:
+                line[i] += line[i + 1]
+            except IndexError:
+                pass
+            i += 1
+        #print line
+        x += 1
+
+
+#@profile
+def print_pascal_triangle_v9(height):
+    if height <= 1:
+        #print ONE_LIST
+        return ONE_LIST
+
+    prev_line = ZERO_LIST + print_pascal_triangle_v9(height - 1) + ZERO_LIST
+    line = [prev_line[i] + prev_line[i + 1] for i in xrange(len(prev_line) - 1)]
+    #print line
+    return line
+
+
+FUNCS = (print_pascal_triangle,
+         print_pascal_triangle_v2,
+         print_pascal_triangle_v3,
+         print_pascal_triangle_v4,
+         print_pascal_triangle_v5,
+         print_pascal_triangle_v6,
+         print_pascal_triangle_v7,
+         print_pascal_triangle_v8,
+         print_pascal_triangle_v9)  #[-1:]
+
+if 0:
+    HEIGHT_SMALL = 5
+    for func in FUNCS:
+        print '%s(%s)' % (func.__name__, HEIGHT_SMALL)
+        for _ in xrange(100):
+            func(HEIGHT_SMALL)
+else:
+    CYCLES = 10000
+    HEIGHT = 20
+    for func in (f.__name__ for f in FUNCS):
+        print '%s(%s) done for %s times in %s seconds' % (func, HEIGHT, CYCLES,
+                timeit.timeit('%s(%s)' % (func, HEIGHT), setup='from __main__ import %s' % func,
+                              number=CYCLES))
