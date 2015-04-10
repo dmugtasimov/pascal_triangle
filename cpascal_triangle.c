57cefa135ab0455d8fa8dc77fe889f0b5a6e25db
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2015-04-10 03:44:48 +0300

    Assembler implementation

--- cpascal_triangle.c
+++ cpascal_triangle.c
@@ -30,8 +30,51 @@ static PyObject* py_cprint_pascal_triangle_v1(PyObject* self, PyObject* args) {
     return Py_None;
 }
 
+
+static PyObject* py_casmprint_pascal_triangle_v2(PyObject* self, PyObject* args) {
+    int height;
+    if (!PyArg_ParseTuple(args, "i", &height)) return NULL;
+
+    int start = height - 1;
+    int size;
+    int index;
+
+    int line[height + 1];
+
+    // Fill in line with 1 and put 0 in the end
+    asm ("movl %0, %%ecx\n\t"
+         "movq %1, %%rdi\n\t"
+         "movl $1, %%eax\n\t"
+         "cld\n\t"
+         "rep\n\t"
+         "stosl\n\t"
+         "movq $0, (%%rdi)\n\t"
+         : /**/
+         : "g" (height), "g" (line)
+         : "eax"
+        );
+
+    for (size = 1; size <= height; size++) {
+        for (index = start + 1; index < height; index++) {
+            line[index] += line[index + 1];
+        }
+        /*
+        for (index = start; index < start + size; index++) {
+            printf("%d ", line[index]);
+        }
+        printf("\n");
+        */
+        start--;
+    }
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+
 static PyMethodDef cpascal_triangle_methods[] = {
     {"cprint_pascal_triangle_v1", py_cprint_pascal_triangle_v1, METH_VARARGS},
+    {"casmprint_pascal_triangle_v2", py_casmprint_pascal_triangle_v2, METH_VARARGS},
     {NULL, NULL}
 };
 
--- perftest.py
+++ perftest.py
@@ -7,23 +7,26 @@ from pascal_triangle import (print_pascal_triangle, print_pascal_triangle_v2,
                              print_pascal_triangle_v9, print_pascal_triangle_v10)
 from cypascal_triangle import (cyprint_pascal_triangle_v1, cyprint_pascal_triangle_v2,
                                cyprint_pascal_triangle_v3)
-from cpascal_triangle import cprint_pascal_triangle_v1
+from cpascal_triangle import cprint_pascal_triangle_v1, casmprint_pascal_triangle_v2
 
 
-FUNCS = (print_pascal_triangle,
-         print_pascal_triangle_v2,
-         print_pascal_triangle_v3,
-         print_pascal_triangle_v4,
-         print_pascal_triangle_v5,
-         print_pascal_triangle_v6,
-         print_pascal_triangle_v7,
-         print_pascal_triangle_v8,
-         print_pascal_triangle_v9,
-         print_pascal_triangle_v10,
-         cyprint_pascal_triangle_v1,
-         cyprint_pascal_triangle_v2,
-         cyprint_pascal_triangle_v3,
-         cprint_pascal_triangle_v1)
+FUNCS = (
+    print_pascal_triangle,
+    print_pascal_triangle_v2,
+    print_pascal_triangle_v3,
+    print_pascal_triangle_v4,
+    print_pascal_triangle_v5,
+    print_pascal_triangle_v6,
+    print_pascal_triangle_v7,
+    print_pascal_triangle_v8,
+    print_pascal_triangle_v9,
+    print_pascal_triangle_v10,
+    cyprint_pascal_triangle_v1,
+    cyprint_pascal_triangle_v2,
+    cyprint_pascal_triangle_v3,
+    cprint_pascal_triangle_v1,
+    casmprint_pascal_triangle_v2,
+)
 
 CYCLES = 10000
 HEIGHT = 20
